9a23f5d67cfb31e6d71eb5cc6c22d04a2e2555e7
Kyle-krn <83144447+Kyle-krn@users.noreply.github.com>
2022-06-29 02:12:57 +0300

    Update medapi.py

--- utils/medapi.py
+++ utils/medapi.py
@@ -2,9 +2,9 @@ import aiohttp
 
 
 class MedApi:
-    url = 'https://sandbox-healthservice.priaid.ch'
-    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNlbWVub2xlZ292aWNoQHlhbmRleC5ydSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTA1NjMiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjItMDQtMTIiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTY1MDY0NjU4MiwibmJmIjoxNjUwNjM5MzgyfQ.95uPAq6bcdJSGavoIhjIFOq76bLevqMX_SopIIIotFU'
-
+    url = config.API_URL
+    token = config.API_TOKEN
+    
     async def get_body_locations(self, language: str):
         # {{host}}/body/locations?token={{token}}&language=ru-ru
         params = {'token': self.token, "language": language}
